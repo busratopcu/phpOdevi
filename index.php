a2bb7d84a4b7332d3ebddbafe58628fe4323f23d
busratopcu <33058168+busratopcu@users.noreply.github.com>
2018-03-22 12:52:34 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Kullanıcı Sistemi</title>
+</head>
+<body>
+<form action="kontrol.php" method="POST" name="giris">
+<label>Öğrenci no:</label><input type="text" name="ogrencno"><br>
+<label>Şifre:</label><input type="password" name="sifre">
+<button type="submit">Gönder</button><br>
+</form>
+</body>
+</html>
